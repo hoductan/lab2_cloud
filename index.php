1664150cc4c3899fe8b4d24d894adc6e7c383766
hoductan <106651892+hoductan@users.noreply.github.com>
2022-09-23 10:15:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
    <title>PHP Test </title>
    </head>
    <body>
+     <h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
    <?php echo '<p> Hello Cloud Computing class! </p>';?>
    </body>
   </html>
